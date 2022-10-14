a6215764b0ffb2ecc762a6a3d0d6b7c483202144
Sabrina Harvey <harveysabrina81@gmail.com>
2022-10-14 13:10:02 -0500

    Added Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,47 @@
+//Exercise 1
 window.onload = function()
 {
-    let board = document.getElementById("board");
-    console.log(board);
-    let squares = document.querySelectorAll("#board div");
-    
-    for(var i in squares)
+    let board = document.getElementById('board');
+    let squares = board.getElementsByTagName('div');
+
+    for(let i of squares)
     {
-        squares[i].classList.add("square");
+        i.classList.add('square');   
     }
 
+   
+    let state = new Array(9);
+    const options = ['X', 'O'];
+    let lastPlayer = "O"
+    let index = 0;
 
+    for (let s of squares)
+    {
+        s.id = index++;
+        s.onclick = (event) =>
+        {
+            //Exercise 2
+            if(lastPlayer === "X")
+            {
+                event.target.innerText = "O";
+                event.target.classList.add("O");
+                state[event.target.id] = "O";
+                lastPlayer = "O";
+            }
+
+            else if(lastPlayer === "O")
+            {
+                event.target.innerText = "X";
+                event.target.classList.add("X");
+                state[event.target.id] = "X";
+                lastPlayer = "X";
+            }
+
+
+
+        }
+    }
     
 
-
-
 }
+
